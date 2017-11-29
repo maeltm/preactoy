4133088d3fdcb2a1f959775269cf98f95bd4643a
Catstail <maeltm18@gmail.com>
2017-11-29 17:58:38 +0900

    Export ports to ENV & Add database uri for test.

--- backend/src/database.js
+++ backend/src/database.js
@@ -7,4 +7,10 @@ mongoose.Promise = bluebird;
 const db = mongoose.connection;
 db.on('error', debug);
 db.once('open', () => { debug('Connected to mongodb server'); });
-mongoose.connect(process.env.DATABASE_URL);
+switch (process.env.NODE_ENV) {
+    case 'test':
+        mongoose.connect(process.env.TEST_DATABASE_URL, { useMongoClient: true });
+        break;
+    default:
+        mongoose.connect(process.env.DATABASE_URL, { useMongoClient: true });
+}
